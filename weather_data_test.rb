c54b60b989e621a9b854e1420dab11c22cb20abf
Anita Kuno <ay.kuno@gmail.com>
2011-01-30 21:07:59 -0500

    Created new files

---
+++ weather_data_test.rb
@@ -0,0 +1,16 @@
+require 'test/unit'
+require 'rubygems'
+require 'shoulda'
+
+class WeatherDataTest < Test::Unit::TestCase
+  should "some test description" do
+    #test body
+  end
+
+  context "When I do something then" do
+    should "behave in some fashion" do
+      #...
+    end
+  end
+end
+
